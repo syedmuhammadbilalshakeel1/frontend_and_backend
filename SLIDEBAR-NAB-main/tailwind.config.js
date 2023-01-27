11fe36b4725256144351aed2a7b6292d2e3cd7e3
Bilalnew1 <120711301+Bilalnew1@users.noreply.github.com>
2023-01-27 18:55:50 +0500

    completed the sinin page and the signup page

--- SLIDEBAR-NAB-main/tailwind.config.js
+++ SLIDEBAR-NAB-main/tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-
-  darkMode:"class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  
     extend: {
-     
-      fontFamily:{
-        Poppins:['Playfair Display', 'serif'],
-        Raleway:['Raleway','sans-serif'],
-      }
+      fontFamily: {
+        Poppins: ["Playfair Display", "serif"],
+        Raleway: ["Raleway", "sans-serif"],
+      },
     },
 
-    extend:{
-      backgroundImage:{
-        'white':"url('/public/white.png')"
-      }
-    }
+    extend: {
+      backgroundImage: {
+        white: "url('/public/white.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
